caa92b9afc29fb203af042a2a14d2a057ca7a999
Denis <denis.basler@gmx.ch>
2023-04-06 10:52:23 +0200

    Typing / Issue #8

--- client/script.js
+++ client/script.js
@@ -1,7 +1,14 @@
 (async () => {
   const myUser = await generateRandomUser();
   let activeUsers = [];
-  let typingUsers = [];
+
+  function getCurrentMessage() {
+    const messages = document.querySelectorAll('.message');
+    if (messages.length > 0) {
+      return messages[messages.length - 1];
+    }
+    return null;
+  }
 
   const socket = new WebSocket(generateBackendUrl());
   socket.addEventListener('open', () => {
@@ -18,13 +25,27 @@
         setTimeout(() => {
           messageElement.classList.add('opacity-100');
         }, 100);
+        setTimeout(() => {
+          const currentMessage = getCurrentMessage();
+          if (currentMessage && currentMessage.type === 'typing') {
+            return;
+          }
+          removeTyping();
+        }, 100);
         break;
       case 'activeUsers':
         activeUsers = message.users;
         break;
       case 'typing':
-        typingUsers = message.users;
-        break;
+        let typingElement = document.getElementById('typing');
+        if (!typingElement) {
+          typingElement = generateTyping(message, myUser);
+          document.getElementById('messages').appendChild(typingElement);
+          setTimeout(() => {
+            typingElement.classList.add('opacity-100');
+          }, 100);
+        }
+        typingElement.querySelector('p').innerHTML = `${message.users.map(u => u.name).join(', ')} is typing...`;        break;
       default:
         break;
     }
